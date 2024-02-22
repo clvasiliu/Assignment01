684562c9b2b81cd856e8aa1536521b1be4b69dc3
Cristian Vasiliu <47012504+clvasiliu@users.noreply.github.com>
2024-02-21 17:22:34 -0800

    Move and complete scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,152 @@
+// STEP 1
+// let someMonth;
+// function theMonth() {};
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// // numeric literal expression
+// 10
+// // string literal expression
+// "string"
+// // Boolean literal expression
+// false
+// // null literal expression
+// null
+
+// STEP 3
+// let complexVariableExpression1 = 12 + 13 * (18 / 2);
+// let complexVariableExpression2 = (9/7) 34 + 25 - (8/4) * 2;
+
+// STEP 4
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let iZipCode;
+// let iYourAge;
+// let strReferralSource;
+// let bMayWeContactYou;
+
+// STEP 5
+// // method 1
+// let iYourAge;
+// let strReferralSource;
+// let bMayWeContactYou;
+// iYourAge = 10;
+// strReferralSource = "LinkedIn";
+// bMayWeContactYou = true;
+//
+// // method 2
+// let iYourAge = 10;
+// let strReferralSource = "LinkedIn";
+// let bMayWeContactYou = true;
+// 
+// //method 3
+// let iYourAge = 10, strReferralSource = "LinkedIn", bMayWeContactYou = true;
+
+// STEP 6
+// let variable;
+// variable = 54 + " is the value of ivariable";
+// console.log(variable);
+
+// STEP 7
+// let variable1;
+// let variable2;
+// variable1 = false + " is the boolean false";
+// console.log(variable1);
+// variable2 = 3 + false;
+// console.log(variable2);
+
+// STEP 8
+// // let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'
+// // console.log(someString)
+// //
+// // The above string literal is not valid because it is missing escape characters.
+// // The issue arises with the apostrophe confusing javascript into thinking you are closing the string.
+// // This issue can be fixed by using an escape character (\) to prevent javascript from reading the apostrophe.
+// // Below is the fixed code.
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+// let nullVariable = null;
+// console.log(nullVariable);
+//
+// let undefinedVariable;
+// console.log(undefinedVariable);
+
+// STEP 10
+// // String
+// let strStringVariable = "string";
+// console.log(typeof(strStringVariable));
+// // Number
+// let iNumberVariable = 10;
+// console.log(typeof(iNumberVariable));
+// // Boolean
+// let bBooleanVariable = true;
+// console.log(typeof(bBooleanVariable));
+// // object
+// let oObjectVariable = Math;
+// console.log(typeof(oObjectVariable));
+// // undefined
+// let undefUndefinedVariable;
+// console.log(typeof(undefUndefinedVariable));
+
+// STEP 11
+// window.alert("Hello " + "Cristian Vasiliu" + ", welcome to the JavaScript class!");
+
+// STEP 12
+// let name = "Cristian Vasiliu";
+// window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13
+// let name = "Cristian Vasiliu";
+// let course = "JavaScript";
+// window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14
+// let name = "Zak Ruvalcaba";
+// let course = "JavaScript";
+// window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 15
+// let name = window.prompt("Input your name: ");
+// let course = "JavaScript";
+// window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 16
+// let name = window.prompt("Input your name: ");
+// let course = window.prompt("Input the class you are taking: ");
+// window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 17
+// let x = 10, y = 20;
+// console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+// STEP 21
+// const iExpectedResult = 196;
+// let iTestVariable = 13 * 13;
+// console.log(iTestVariable != iExpectedResult);
+
+// STEP 22
+// const iExpectedResult = 169;
+// let iTestVariable = 14 * 14;
+// console.log(iTestVariable == iExpectedResult);
